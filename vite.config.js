7870e907609221ae666cd1fabe09a8591bd1f358
oyezaifi <huzaifaaaa18@gmail.com>
2025-03-25 02:25:24 +0500

    added things

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Ensures correct asset paths
+  base: "/", // Ensures correct asset paths
   build: {
     outDir: "dist", // Ensure output matches Vercel's expected directory
   },
